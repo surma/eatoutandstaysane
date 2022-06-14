b7ab9ca34a3136ed843b0d09cc203f4a0e9690f0
Surma <surma@surma.dev>
2022-06-14 23:21:10 +0100

    Make decalorify more robust

--- main.js
+++ main.js
@@ -1,55 +1,55 @@
-import {getDocument, GlobalWorkerOptions} from "pdfjs-dist";
+import { getDocument, GlobalWorkerOptions } from "pdfjs-dist";
 import pdfWorkerURL from "pdfjs-dist/build/pdf.worker.js?url";
 
 GlobalWorkerOptions.workerSrc = pdfWorkerURL;
 
-const {canvases, file, log} = document.all;
+const { canvases, file, log } = document.all;
 
 const FACTOR = 3;
 
 function decalorify(str) {
-	return str.replace(/[0-9\.]+k?cal/i, "");
+  return str.replace(/[^\s]*\s*k?cals?[^\w]/i, "");
 }
 
 function createCanvas(width, height) {
-	const canvas = document.createElement("canvas");
-	canvases.append(canvas);
-	canvas.classList.add("page");
-	canvas.width = width * FACTOR;
-	canvas.height = height * FACTOR;
-	const ctx = canvas.getContext("2d");
-	ctx.scale(FACTOR, FACTOR);
-	return ctx;
+  const canvas = document.createElement("canvas");
+  canvases.append(canvas);
+  canvas.classList.add("page");
+  canvas.width = width * FACTOR;
+  canvas.height = height * FACTOR;
+  const ctx = canvas.getContext("2d");
+  ctx.scale(FACTOR, FACTOR);
+  return ctx;
 }
 
 async function onFileSelect(ev) {
-	const file = ev.target?.files?.[0];
-	if(!file) return;
-	try {
-		const data = await new Response(file).arrayBuffer();
-		const document = await getDocument({data, }).promise;
-		for(let i = 1; i <= document.numPages; i++) {
-			const page = await document.getPage(i);
-			let [x, y, width, height] = page.view;
-			width -= x;
-			height -= y;
-			const ctx = createCanvas(width, height);
-			const textStream = page.streamTextContent();
-			const reader = textStream.getReader();
-			while(true) {
-				const {value, done} = await reader.read();
-				if(done) break;
-				const {items, styles} = value;
-				for(const item of items) {
-					ctx.font = `${item.height}px sans-serif`;
-					const str = decalorify(item.str);
-					ctx.fillText(str, item.transform[4], item.transform[5]);
-				}
-			}
-		}
-	} catch(e) {
-		log.innerHTML = `Error: ${e.message}`;
-	}
+  const file = ev.target?.files?.[0];
+  if (!file) return;
+  try {
+    const data = await new Response(file).arrayBuffer();
+    const document = await getDocument({ data }).promise;
+    for (let i = 1; i <= document.numPages; i++) {
+      const page = await document.getPage(i);
+      let [x, y, width, height] = page.view;
+      width -= x;
+      height -= y;
+      const ctx = createCanvas(width, height);
+      const textStream = page.streamTextContent();
+      const reader = textStream.getReader();
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) break;
+        const { items, styles } = value;
+        for (const item of items) {
+          ctx.font = `${item.height}px sans-serif`;
+          const str = decalorify(item.str);
+          ctx.fillText(str, item.transform[4], item.transform[5]);
+        }
+      }
+    }
+  } catch (e) {
+    log.innerHTML = `Error: ${e.message}`;
+  }
 }
 
 file.addEventListener("change", onFileSelect);
